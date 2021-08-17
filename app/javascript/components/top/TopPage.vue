2459ec7f9b8335060094a0fedac35503539a565c
sou7322 <skoike322@gmail.com>
2021-08-17 23:21:41 +0900

    [Add] 仮のトップページを作成
    新規ユーザー登録画面へのリンクだけを持った、仮のトップページを作成した。

---
+++ app/javascript/components/top/TopPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <p>{{ title }}</p>
+    <p><router-link :to="{ name: 'RegisterPage' }">to register</router-link></p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      title: 'Dummy Top'
+    }
+  } 
+}
+</script>
