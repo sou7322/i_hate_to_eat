90bb12ce49f922915ed4662ba31a88dd4b96cf1f
sou7322 <skoike322@gmail.com>
2021-08-21 12:32:16 +0900

    [Fix] Userテーブル作成のマイグレーションファイルを修正
    `role`カラムのデフォルト値設定について、タイポにより設定が正しくされていなかった。
    今回はマイグレーションファイルを修正の後、`db:migrate:reset`を行った。

--- db/migrate/20210815083324_create_users.rb
+++ db/migrate/20210815083324_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :email, null: false
       t.string :crypted_password
       t.string :salt
-      t.integer :role, null:false, defaut: 0
+      t.integer :role, null:false, default: 0
 
       t.timestamps
       t.index :email, unique: true
