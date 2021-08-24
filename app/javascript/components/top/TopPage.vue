91319f6aecc5799761755bdd9fcfaf00f235e883
sou7322 <skoike322@gmail.com>
2021-08-24 11:05:46 +0900

    [Fix] htmlタグを整理
    <v-app> <v-main>の位置を修正した。

--- app/javascript/components/top/TopPage.vue
+++ app/javascript/components/top/TopPage.vue
@@ -1,14 +1,12 @@
 <template>
-  <v-app>
-    <v-main>
-      <h1>{{ title }}</h1>
-      <p>
-        <router-link :to="{ name: 'RegisterPage' }">
-          to register
-        </router-link>
-      </p>
-    </v-main>
-  </v-app>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>
+      <router-link :to="{ name: 'RegisterPage' }">
+        to register
+      </router-link>
+    </p>
+  </div>
 </template>
 
 <script>
