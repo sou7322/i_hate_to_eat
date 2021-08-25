68e10390db3f2d0e23353c1e2b9253d58408dc1c
sou7322 <skoike322@gmail.com>
2021-08-25 15:28:02 +0900

    [Fix] userモデルのバリデーションの記述をリファクタリング
    `password` `password_confirmation`にかけるバリデーションの条件式を
    プライベートメソッドにまとめた。

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,15 @@ class User < ApplicationRecord
     validates :name, length: { maximum: 50 }
     validates :email, uniqueness: true
   end
-  validates :password, length: { minimum: 5 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 5 }, if: :new_or_changes_password
+  validates :password, confirmation: true, if: :new_or_changes_password
+  validates :password_confirmation, presence: true, if: :new_or_changes_password
+
+  private
+
+    def new_or_changes_password
+      new_record? || changes[:crypted_password]
+    end
 end
 
 # == Schema Information
