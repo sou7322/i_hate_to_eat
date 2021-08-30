95d8e00e4c8ab84e3569dff6c8ce573e2fcb7dec
sou7322 <skoike322@gmail.com>
2021-08-30 23:52:12 +0900

    [Add] ヘッダーの大枠を作成
    スペースの確認のためにヘッダーの大枠だけを作成、まだ中身はない。

---
+++ app/javascript/components/commons/CommonHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app-bar
+    app
+    color="blue"
+    flat
+    fixed
+    height="100"
+  >
+    <v-row>
+      <flash-message />
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card
+          color="red"
+          flat
+          width="300px"
+          height="50px"
+        >
+          <div class="text-center text-h3">
+            Ad
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-app-bar>
+</template>
+
+<script>
+import FlashMessage from './FlashMessage';
+
+export default {
+  components: {
+    FlashMessage
+  }
+};
+</script>
