3a2d1668a263690517a82071c88420868cedc5ed
sou7322 <skoike322@gmail.com>
2021-08-31 14:44:31 +0900

    [Add] activeadminの初期ファイルを作成
    `rails g active_admin:install --skip-users --use_webpacker`を実行。
    設定ファイルなど初期ファイルの生成を行なった。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
   root to: 'top#index'
 
   namespace :api, format: 'json' do
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,5 @@
 const { environment } = require('@rails/webpacker')
+const jquery = require('./plugins/jquery')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 const eslint = require('./loaders/eslint')
@@ -6,4 +7,5 @@ const eslint = require('./loaders/eslint')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 environment.loaders.append('eslint', eslint)
+environment.plugins.prepend('jquery', jquery)
 module.exports = environment
