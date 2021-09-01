1c133a55d13c66c7a2659e3e3a061506c6bda4a3
sou7322 <skoike322@gmail.com>
2021-09-01 22:29:14 +0900

    [Add] バリデーションとインスタンスメソッドを追加
    BMR計算用に追加したカラムにバリデーションを追加。
    またインスタンスメソッドとして、生年月日から現在の年齢を計算するメソッド、
    BMRを計算するメソッドを追加。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,40 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  enum gender: { female: 0, male: 10 }
   enum role: { general: 0, admin: 10 }
 
   with_options presence: true do
     validates :name, length: { maximum: 50 }
     validates :email, uniqueness: true
+    validates :role
+    validates :gender
+    validates :height, numericality: { only_integer: true }
+
+    with_options numericality: true do
+      validates :weight
+      validates :bmr
+    end
   end
   validates :password, length: { minimum: 5 }, if: :new_or_changes_password
   validates :password, confirmation: true, if: :new_or_changes_password
   validates :password_confirmation, presence: true, if: :new_or_changes_password
 
   private
+
+    def calc_bmr
+      age = calc_age
+
+      if gender == 'female'
+        9.247 * weight + 3.098 * height - 4.33 * age + 447.593
+      else
+        13.397 * weight + 4.799 * height - 5.677 * age + 88.362
+      end
+    end
+
+    def calc_age
+      (Time.zone.today.strftime("%Y%m%d").to_i - birth.strftime("%Y%m%d").to_i) / 10_000
+    end
 
     def new_or_changes_password
       new_record? || changes[:crypted_password]
