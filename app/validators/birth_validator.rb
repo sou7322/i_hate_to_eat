f9968d0af2c118d205fd9ff03ce581cfe39561de
sou7322 <skoike322@gmail.com>
2021-09-03 20:15:39 +0900

    [Add] userモデルスペックに項目を追加
    userモデルのモデルスペックに、今回追加した、生年月日・身長・体重
    ・bmrカラムに関する項目を追加。

--- app/validators/birth_validator.rb
+++ app/validators/birth_validator.rb
@@ -2,13 +2,9 @@ class BirthValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return unless value
 
-    if value.future?
-      record.errors.add(:base, "未来の日付は入力できません")
-    end
+    record.errors.add(:birth, "未来の日付は入力できません") if value.future?
 
     age = record.calc_age
-    if age < 18 || age > 60
-      record.errors.add(:base, "本サービスは18歳以上60歳未満の方が対象です")
-    end
+    record.errors.add(:birth, "本サービスは18歳以上60歳未満の方が対象です") if age < 18 || age >= 60
   end
 end
