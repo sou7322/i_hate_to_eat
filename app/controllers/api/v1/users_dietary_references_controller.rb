fd8dc77d32c4dc63e5c837f471247585de185507
sou7322 <skoike322@gmail.com>
2021-09-10 00:03:10 +0900

    [Add] 食事摂取基準に関わるアクションを作成
    `users_dietary_references_controller`を作成し、show updateアクションを
    実装した。なお、ユーザー新規登録時に性別・生年月日も合わせて登録するよう
    変更することを考え、ユーザーごとに該当する食事摂取基準を判別する処理は
    concernに切り出した。

---
+++ app/controllers/api/v1/users_dietary_references_controller.rb
@@ -0,0 +1,36 @@
+module Api
+  module V1
+    class UsersDietaryReferencesController < ApplicationController
+      include Api::DietaryReferenceIntakesHandler
+      before_action :set_user
+
+      def show
+        dri = @user.dietary_reference_intake
+        json_string = DietaryReferenceIntakeSerializer
+                        .new(dri)
+                        .serialized_json
+        render json: json_string
+      end
+
+      def update
+        dri = set_reference_intake(@user)
+        dri_id = dri.id
+
+        if @user.update(dietary_reference_intake_id: dri_id)
+          json_string = DietaryReferenceIntakeSerializer
+                          .new(@user.dietary_reference_intake)
+                          .serialized_json
+          render json: json_string
+        else
+          render400(nil, @user.errors.full_messages)
+        end
+      end
+
+      private
+
+        def set_user
+          @user = User.find(current_user.id)
+        end
+    end
+  end
+end
--- app/models/dietary_reference_intake.rb
+++ app/models/dietary_reference_intake.rb
@@ -1,4 +1,77 @@
 class DietaryReferenceIntake < ApplicationRecord
+  # Associations
+  has_many :users, dependent: :nullify
+  
+  # Enums
+  enum gender: { female: 0, male: 10 }
+
+  # Scopes
+  scope :for_female, -> { where(gender: 'female') }
+  scope :for_maele, -> { where(gender: 'male') }
+
+  # Validations
+  with_options presence: true do
+    validates :gender
+
+    with_options numericality: true do
+      validates :age_bottom
+      validates :age_top
+      validates :biotin
+      validates :calcium
+      validates :chromium
+      validates :copper
+      validates :folate
+      validates :iodine
+      validates :iron
+      validates :magnesium
+      validates :manganese
+      validates :molybdenum
+      validates :niacin
+      validates :pantothenic_acid
+      validates :phosphorus
+      validates :potassium
+      validates :selenium
+      validates :upper_limit_calcium
+      validates :upper_limit_chromium
+      validates :upper_limit_copper
+      validates :upper_limit_folate
+      validates :upper_limit_iodine
+      validates :upper_limit_iron
+      validates :upper_limit_manganese
+      validates :upper_limit_molybdenum
+      validates :upper_limit_niacin
+      validates :upper_limit_phosphorus
+      validates :upper_limit_selenium
+      validates :upper_limit_vitamin_a
+      validates :upper_limit_vitamin_b6
+      validates :upper_limit_vitamin_d
+      validates :upper_limit_vitamin_e
+      validates :upper_limit_zinc
+      validates :vitamin_a
+      validates :vitamin_b1
+      validates :vitamin_b12
+      validates :vitamin_b2
+      validates :vitamin_b6
+      validates :vitamin_c
+      validates :vitamin_d
+      validates :vitamin_e
+      validates :vitamin_k
+      validates :zinc
+    end
+  end
+
+  # Class methods
+  def self.for_eighteen_to_twentynine
+    find_by(age_bottom: 18)
+  end
+
+  def self.for_thirty_to_fortynine
+    find_by(age_bottom: 30)
+  end
+
+  def self.for_fifty_to_sixtyfour
+    find_by(age_bottom: 50)
+  end
 end
 
 # == Schema Information
