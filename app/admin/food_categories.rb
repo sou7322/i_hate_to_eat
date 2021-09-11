e52da270806ea1e9af383dbc19f56d927d53b527
sou7322 <skoike322@gmail.com>
2021-09-11 17:42:18 +0900

    [Add] activeadminの管理対象に追加
    食品カテゴリについて、activeadminの管理画面を作成。

---
+++ app/admin/food_categories.rb
@@ -0,0 +1,32 @@
+ActiveAdmin.register FoodCategory do
+  permit_params :name
+
+  # 一覧ページの表示項目
+  index do
+    selectable_column
+    id_column
+    column(:name)
+    actions
+  end
+
+  # 一覧ページのフィルター項目
+  filter :name
+
+  # 詳細ページの表示項目
+  show do
+    attributes_table do
+      row(:id)
+      row(:name)
+      row(:created_at)
+      row(:updated_at)
+    end
+  end
+
+  # フォームの入力項目
+  form do |f|
+    f.inputs do
+      f.input :name
+    end
+    f.actions
+  end
+end
