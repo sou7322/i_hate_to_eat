cd989bc713a6c3df41dc6e1fee6c67e8152b7d10
sou7322 <skoike322@gmail.com>
2021-09-11 21:49:15 +0900

    [Add] 食品カテゴリー一覧画面を作成
    食品カテゴリー一覧画面を作成。ボタンにはしているが、まだリンク先は
    設定していない。

---
+++ app/javascript/components/pages/FoodIndex.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-tabs vertical>
+    <v-tab>
+      カテゴリー一覧
+    </v-tab>
+    <v-tab>
+      タグ一覧
+    </v-tab>
+
+    <v-tab-item>
+      <food-index-categories />
+    </v-tab-item>
+    <v-tab-item>
+      <p>
+        comming soon...
+      </p>
+    </v-tab-item>
+  </v-tabs>
+</template>
+
+<script>
+import FoodIndexCategories from '../parts/FoodIndexCategories';
+
+export default {
+  components: {
+    FoodIndexCategories
+  }
+};
+</script>
