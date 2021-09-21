4aca47917e94428194f4644bf974185dfb985095
sou7322 <skoike322@gmail.com>
2021-09-21 19:54:13 +0900

    [Add] Suggestionモデルの管理画面を作成
    activeadminの管理下にSuggestionモデルを追加。
    管理画面を作成した。

---
+++ app/admin/suggestions.rb
@@ -0,0 +1,30 @@
+ActiveAdmin.register Suggestion do
+  permit_params :user_id, :food_id, :amount, :target_date, :expires_at
+
+  # Index
+  index do
+    selectable_column
+    id_column
+    column(:user_id)
+    column(:food_id)
+    column(:target_date)
+    column(:expires_at)
+    actions
+  end
+
+  # Index filter
+  filter :user_id
+  filter :expires_at
+
+  # Show
+  show do
+    attributes_table do
+      row(:id)
+      row(:user_id)
+      row(:food_id)
+      row(:amuount)
+      row(:target_date)
+      row(:expires_at)
+    end
+  end
+end
